0374789fae90e8acce1566db993db491e6ed2a0b
Snehavellanki <vellankisneha@gmail.com>
2021-08-13 17:09:24 -0400

    Add files via upload

---
+++ Models/TypesOfSearchKeys.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartNameSearch.Models
+{[BindProperties]
+    public class TypesOfSearchKeys
+    {
+        [StringLength(25)]
+        public string GlobalSearch { get; set; }
+        [StringLength(25)]
+        public string SearchFirstName { get; set; }
+        [StringLength(25)]
+        public string SearchMiddleName { get; set; }
+        [StringLength(25)]
+        public string SearchLastName { get; set; }
+
+    }
+}
