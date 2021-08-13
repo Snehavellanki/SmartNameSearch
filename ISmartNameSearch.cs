887863c1b5bc9e96c92449143a4205865c5b4d1f
Snehavellanki <vellankisneha@gmail.com>
2021-08-13 17:02:12 -0400

    Add files via upload

---
+++ ISmartNameSearch.cs
@@ -0,0 +1,14 @@
+﻿using SmartNameSearch.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartNameSearch
+{
+    public interface ISmartNameSearch
+    {
+        IEnumerable<People> SearchNames(TypesOfSearchKeys SearchKeyword, string typeOfSearch);
+        List<PeopleNames> AutoSearchPeoples(string keyword);
+    }
+}
